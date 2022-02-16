ffc45de533e6aab3e9ee8ad833a386a3a1889b3b
Nurzat <nurzat.izakanova.ss@gmail.com>
2022-02-16 12:53:55 +0600

    task8

---
+++ loan_calc/js-lesson-materials/04-basics/js/script.js
@@ -0,0 +1,68 @@
+// условие ? выражение1 : выражение2
+
+// let user = "petee";
+
+// user == "pete" ? "Pete" : "Not Pete";
+
+// greet("john");
+
+// function greet(name = "Unknown user") {
+//   console.log(`Hello ${name}`);
+// }
+
+// const hi = function () {
+//   console.log(`Hello`);
+// };
+
+// hi();
+
+// const sum = (a, b) => {
+//   let result = a + b;
+//   return result;
+// };
+
+// console.log(sum(10, 10));
+
+// const divide = (a, b) => a / b;
+// console.log(divide(10, 2));
+
+// let i = 0;
+// while (i < friends.length) {
+//   console.log(friends[i].toUpperCase());
+
+//   i++;
+// }
+
+// for (let i = 0; i < friends.length; i++) {
+//   if (friends[i] === "pete") {
+//     continue;
+//   } else {
+//     console.log(friends[i]);
+//   }
+// }
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 === 0) {
+//     if (i === 10) {
+//       continue;
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+// let i = 12;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+const friends = ["masha", "john", "mary", "akmat", "pete", "tashmat"];
+
+friends.forEach((friend) => console.log(friend));
+
+// foreach()
+// map()
+// reduce()
+// filter()
